796b28f8d667a1552254027de0dbd2e9244fd87d
aliayan15 <79214171+aliayan15@users.noreply.github.com>
2024-01-26 22:20:17 +0200

    Update

---
+++ GGJ Game/Assets/Scripts/SOs/SOGameData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Game Data")]
+public class SOGameData : ScriptableObject
+{
+    
+}
